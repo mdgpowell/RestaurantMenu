1efa77453c2d5e54b2473cfe1d708cfe672fde57
marcuspowell <mdgpowell@gmail.com>
2023-01-12 19:23:43 -0600

    did preliminary outline

--- src/main/java/Restaurant/Menu.java
+++ src/main/java/Restaurant/Menu.java
@@ -1,4 +1,22 @@
 package Restaurant;
+import java.util.ArrayList;
+import java.util.Date;
 
+/*
+Design Outline:
+
+Class Menu
+	Field [private]: menuItems (ArrayList<MenuItem>)
+	Field [private]: lastUpdated (Date)
+	Methods: Getters and Setters
+Class MenuItem
+	Field [private]: price (double)
+	Field [private]: description (String)
+	Field [private]: category (String)
+	Field [private]: new (boolean)
+	Methods: Getters and Setters
+ */
 public class Menu {
+    private ArrayList<MenuItem> menuItems;
+    private Date lastUpdated;
 }
--- src/main/java/Restaurant/MenuItem.java
+++ src/main/java/Restaurant/MenuItem.java
@@ -1,4 +1,36 @@
 package Restaurant;
 
 public class MenuItem {
+    private double price;
+    private String description;
+    private String category;
+    private boolean newItem;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNewItem() {
+        return newItem;
+    }
 }
