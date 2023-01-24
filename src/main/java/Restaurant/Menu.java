8a67dcbdef1bca29fcb6bbb5cb432fd0cb352cc5
marcuspowell <mdgpowell@gmail.com>
2023-01-23 19:13:09 -0600

    did outline part 2

--- src/main/java/Restaurant/Menu.java
+++ src/main/java/Restaurant/Menu.java
@@ -9,14 +9,33 @@ Class Menu
 	Field [private]: menuItems (ArrayList<MenuItem>)
 	Field [private]: lastUpdated (Date)
 	Methods: Getters and Setters
+	Instance Methods:
+	    public void addItem(menuItem)
+	    public void removeItem(menuItem)
+	    public static Date getLastUpdated()
+	    public String toString()
 Class MenuItem
 	Field [private]: price (double)
 	Field [private]: description (String)
 	Field [private]: category (String)
 	Field [private]: new (boolean)
 	Methods: Getters and Setters
+	Instance Method:
+	    public boolean isNew()
+	    public String toString()
+	    public boolean equals()
  */
 public class Menu {
     private ArrayList<MenuItem> menuItems;
-    private Date lastUpdated;
+    private static Date lastUpdated;
+
+    public void addItem(MenuItem item) {
+
+    }
+    public void removeItem(MenuItem item) {
+
+    }
+    public static Date getLastUpdated() {
+        return lastUpdated;
+    }
 }
